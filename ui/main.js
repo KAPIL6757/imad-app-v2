82e4fa854f8c32aaa9421496bcd414a244d99910
KAPIL6757 <kapil3355@outlook.com>
2017-05-26 17:19:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,15 @@
 console.log('Loaded!');
+//change the text when click
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+//move to image on right 
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 15;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function () {
-   var interval = setInterval(moveRight, 100);
+   var interval = setInterval(moveRight, 50);
 };
