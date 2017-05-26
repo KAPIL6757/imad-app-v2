46331feb7d53846e3e692ad8789414d14478f2a4
KAPIL6757 <kapil3355@outlook.com>
2017-05-27 01:02:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,21 @@ var nameInput = document.getElementById('name');
  var name = nameInput.value;
  var submit = document.getElementById('submit_btn');
  submit.onclick = function () {
-     var names = ['name1','name2','name3','name4'];
+     
+     
+     //craete a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store in the variable
+    request.onreadystatechange = function() 
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+        //take some action
+        if(request.status === 200)
+        {
+                var names = request.response.Text;
+     name = JSON.parse(names);
      var list = '';
      for(var i=0; i<names.length; i++)
      {
@@ -35,5 +49,15 @@ var nameInput = document.getElementById('name');
          
      }
  var ul = document.getElementById('namelist');
- ul.innerHTML = list;
+} ul.innerHTML = list;
+
  };
+           
+    };
+    //make the request
+    request.open('GET','http://kapil6757.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    };
+     
+     
+     
