dd9b23b5cfeb5b0afa4ddc715784342f0b9b6255
KAPIL6757 <kapil3355@outlook.com>
2017-05-26 13:24:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 //change the text of main-text div
 
-var element = document.getElementByID('main-text');
+var element = document.getelementbyid('main-text');
 
 element.innerHTML = 'new value';
 
-var img = document.getElementByID('madi');
+var img = document.getelementbyid('madi');
 img.onclick = function() {
   img.style.marginleft = '100px';
 };
