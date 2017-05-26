92e3150dbdf05a295caa5ea4e25547d18b746e10
KAPIL6757 <kapil3355@outlook.com>
2017-05-27 00:05:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,17 @@ button.onclick = function () {
     request.send(null);
     
 };
+var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('submit_btn');
+ submit.onclick = function(){
+     var name = ['name1','name2','name3','name4'];
+     var list = '';
+     for(var i=0;i<names.length;i++)
+     {
+         list += '<li>' + names[i] + '</li>';
+         
+     }
+ var ul = document.getElementById('name list');
+ ul.innerHTML = list;
+ };
