d100c49091b24eb79523407cce0e738c18a39576
KAPIL6757 <kapil3355@outlook.com>
2017-05-27 01:08:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick = function () {
     request.send(null);
     
 };
-var nameInput = document.getElementById('name');
- var name = nameInput.value;
+
  var submit = document.getElementById('submit_btn');
  submit.onclick = function () {
      
@@ -51,9 +50,11 @@ var nameInput = document.getElementById('name');
  var ul = document.getElementById('namelist');
 ul.innerHTML = list;
 }
- };
+ }
            
     };
+    var nameInput = document.getElementById('name');
+ var name = nameInput.value;
     //make the request
     request.open('GET','http://kapil6757.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
