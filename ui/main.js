ce94f1a24fb6715d0cbb64c192205f5a1c5b1b75
KAPIL6757 <kapil3355@outlook.com>
2017-05-26 23:15:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function () {
         }
     };
     //make the request
-    request.open('GET','http://kapil6757.imad.hasura-app.io/',true);
+    request.open('GET','http://kapil6757.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
